18bb9783d2f1fc76d6886022e1cdf785fcd3273c
Kana <kanat3108@gmail.com>
2017-06-08 01:31:45 +0300

    adding test

---
+++ app/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  	$(".owl-carousel").owlCarousel({
+  		margin: 20,
+  		stagePadding: 20,
+  		items: 3,
+  		nav: true,
+  		navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+  		loop: true
+  		
+	});
+});
+
